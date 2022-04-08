813f42a2039c9643ce484039f7bf6c2c0f67c297
maria.nunez <maria.nunez@wefox.com>
2022-04-08 13:36:21 +0200

    validate pass and email, refactor

--- app/src/main/java/com/marianunez/chatapp/LoginActivity.kt
+++ app/src/main/java/com/marianunez/chatapp/LoginActivity.kt
@@ -1,84 +1,98 @@
 package com.marianunez.chatapp
 
-import android.content.Context
-import android.content.Intent
-import android.graphics.Color
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.auth.FirebaseAuth
+import com.marianunez.chatapp.commons.startActivity
 import com.marianunez.chatapp.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
 
-    // 1. declare Firebase Authentication
-    private lateinit var auth: FirebaseAuth
-
     private lateinit var binding: ActivityLoginBinding
+    private lateinit var auth: FirebaseAuth // 1. declare Firebase Authentication
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // 2. initialize FirebaseAuth
-        auth = FirebaseAuth.getInstance()
+        auth = FirebaseAuth.getInstance() // 2. initialize FirebaseAuth
 
         initUi()
-        handleLogIn()
     }
 
     private fun initUi() {
+        if (auth.currentUser != null) {
+            goToChats()
+        }
+
         binding.logInButton.setOnClickListener {
-            when {
-                !isEmailValid(binding.emailInput.text.toString()) -> binding.emailInput.setBackgroundColor(
-                    Color.RED
-                )
-                !isPasswordValid(binding.passwordInput.text.toString()) -> binding.passwordInput.setBackgroundColor(
-                    Color.RED
-                )
-                else -> {
-                    binding.emailInput.setBackgroundColor(Color.TRANSPARENT)
-                    binding.passwordInput.setBackgroundColor(Color.TRANSPARENT)
-                    binding.loading.visibility = View.VISIBLE
-
-
-                    auth.signInWithEmailAndPassword(binding.emailInput.text.toString(), binding.passwordInput.text.toString()).addOnCompleteListener { task ->
-                        if (task.isComplete && task.isSuccessful) {
-                            goToChats()
-                            Toast.makeText(this, "Done", Toast.LENGTH_LONG).show()
-                        } else {
-                            binding.loading.visibility = View.GONE
-                            Toast.makeText(this, "Something went wrong", Toast.LENGTH_LONG).show()
-                        }
-                    }
-                }
-            }
+            handleLogIn()
         }
 
         binding.signUpButton.setOnClickListener {
-            Intent(this, SignUpActivity::class.java).also {
-                startActivity(it)
-            }
+            startActivity<SignUpActivity>()
+        }
+
+        binding.forgotPassword.setOnClickListener{
+            resetPassword()
         }
     }
 
     private fun handleLogIn() {
-        if (auth.currentUser != null) {
-            goToChats()
+        when {
+            !isEmailValid(binding.emailInput.text.toString()) -> binding.emailLayout.error = "Email not valid"
+            !isPasswordValid(binding.passwordInput.text.toString()) -> binding.passwordLayout.error = "Password not valid"
+            else -> {
+                binding.apply {
+                    loading.visibility = View.VISIBLE
+                }
+                handleAuthentication()
+            }
         }
     }
 
     private fun isEmailValid(email: String): Boolean = email.contains("@") && email.isNotBlank()
     private fun isPasswordValid(password: String): Boolean = password.length > 7
 
-    private fun goToChats() {
-        Intent(this, ChatActivity::class.java).also {
-            startActivity(it)
-            finish()
+    private fun handleInputFields(){
+        // TODO
+    }
+
+    // Firebase things
+    private fun handleAuthentication() {
+        auth.signInWithEmailAndPassword(
+            binding.emailInput.text.toString(),
+            binding.passwordInput.text.toString()
+        ).addOnCompleteListener { task ->
+            if (task.isComplete && task.isSuccessful) {
+                goToChats()
+                Toast.makeText(this, "Done", Toast.LENGTH_LONG).show()
+            } else {
+                binding.loading.visibility = View.GONE
+                Toast.makeText(this, "Something went wrong", Toast.LENGTH_LONG).show()
+            }
         }
     }
+
+    private fun resetPassword(){
+        auth.sendPasswordResetEmail(
+            binding.emailInput.text.toString()
+        ).addOnCompleteListener { task ->
+            if (task.isComplete && task.isSuccessful) {
+                Toast.makeText(this, "Recovery password email sent", Toast.LENGTH_LONG).show()
+            } else {
+                Toast.makeText(this, "Something went wrong", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+    // End of Firebase things
+
+    private fun goToChats() {
+        startActivity<ChatActivity>()
+        finish()
+    }
+
 }
