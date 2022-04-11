eaaeef6ca4d21aae18249f39188d31170ed078ce
maria.nunez <maria.nunez@wefox.com>
2022-04-11 13:18:41 +0200

    create dialog with compose

--- app/src/main/java/com/marianunez/chatapp/fragments/CreateChatDialogFragment.kt
+++ app/src/main/java/com/marianunez/chatapp/fragments/CreateChatDialogFragment.kt
@@ -4,8 +4,36 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.Button
+import androidx.compose.material.Card
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Close
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.ViewCompositionStrategy
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import androidx.fragment.app.DialogFragment
 import com.marianunez.chatapp.databinding.FragmentCreateChatDialogBinding
+import com.marianunez.chatapp.ui.theme.ChatAppTheme
 
 class CreateChatDialogFragment : DialogFragment(), View.OnClickListener {
 
@@ -23,39 +51,95 @@ class CreateChatDialogFragment : DialogFragment(), View.OnClickListener {
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentCreateChatDialogBinding.inflate(inflater, container, false)
+        binding.modalCompose.apply {
+            setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
+            setContent {
+                ChatAppTheme {
+                    Card(
+                        modifier = Modifier.wrapContentSize()
+                    ) {
+                        Modal()
+                    }
+                }
+            }
+        }
         return binding.root
     }
 
-    override fun onResume() {
-        super.onResume()
-
-        initLayout()
-    }
-
-    private fun initLayout(){
-        // this is to set the width and height of the dialog
-        dialog?.window?.setLayout(
-            ViewGroup.LayoutParams.MATCH_PARENT,
-            ViewGroup.LayoutParams.WRAP_CONTENT
-        )
-    }
-
-    override fun onClick(v: View) {
-        when (v) {
-            binding.createButton -> onCreateClicked()
-            binding.closeButton -> onCloseClicked()
+    @Composable
+    fun Modal() {
+        Column {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Title()
+                CloseButton()
+            }
+            InputTextField("Enter name")
+            InputTextField("Enter description")
+            ModalButton(text = "Create")
         }
     }
 
-    private fun onCloseClicked() {
-        binding.closeButton.setOnClickListener(this)
-        binding.inputChatName.text.clear()
-        binding.inputChatDescription.text.clear()
-        dismiss()
+    @Composable
+    fun InputTextField(label: String) {
+        // this var will be observed by this composable function
+        var text by remember {
+            mutableStateOf("")
+        }
+        OutlinedTextField(
+            value = text,
+            onValueChange = { text = it },
+            label = { Text(text = label) },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
+            modifier = Modifier.fillMaxWidth()
+        )
+    }
+
+    @Composable
+    fun Title() {
+        Text(text = "Create a new chat", fontWeight = FontWeight.Bold, fontSize = 24.sp)
+    }
+
+    @Composable
+    fun ModalButton(text: String) {
+        Button(onClick = { onCreateClicked() }, Modifier.fillMaxWidth()) {
+            Text(text = text)
+        }
+    }
+
+    @Composable
+    fun CloseButton() {
+        IconButton(onClick = { dismiss() }) {
+            Icon(
+                Icons.Outlined.Close,
+                contentDescription = "Close button",
+                Modifier.wrapContentSize(),
+                Color.Black
+            )
+        }
+    }
+
+    @Preview(showBackground = true)
+    @Composable
+    fun DefaultPreview() {
+        ChatAppTheme {
+            Modal()
+        }
+    }
+
+    override fun onClick(v: View) {
+        // TODO
     }
 
     private fun onCreateClicked() {
         //TODO
     }
 
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
