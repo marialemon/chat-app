11ef896db92deb69bae5444c0c3c0aed3afe517a
maria.nunez <maria.nunez@wefox.com>
2022-04-26 09:09:26 +0200

    add methods to make snapshot up to date

--- app/src/main/java/com/marianunez/chatapp/ui/adapters/FirestoreAdapter.kt
+++ app/src/main/java/com/marianunez/chatapp/ui/adapters/FirestoreAdapter.kt
@@ -1,6 +1,7 @@
 package com.marianunez.chatapp.ui.adapters
 
 import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.firestore.DocumentChange
 import com.google.firebase.firestore.DocumentSnapshot
 import com.google.firebase.firestore.EventListener
 import com.google.firebase.firestore.ListenerRegistration
@@ -16,9 +17,11 @@ abstract class FirestoreAdapter<ViewHolder : RecyclerView.ViewHolder?>(private v
     private val snapshots = ArrayList<DocumentSnapshot>()
 
     companion object {
+        // tag to use in the logs
         const val TAG = "Firestore Adapter"
     }
 
+    // we are going to use these methods as soon as we want the adapters to start and stop listening the changes
     fun startListening() {
         if (registration == null) {
             registration = query.addSnapshotListener(this)
@@ -32,6 +35,27 @@ abstract class FirestoreAdapter<ViewHolder : RecyclerView.ViewHolder?>(private v
         }
         snapshots.clear()
         notifyDataSetChanged()
+    }
+
+    private fun onDocumentAdded(change: DocumentChange) {
+        snapshots.add(change.newIndex, change.document)
+        notifyItemInserted(change.newIndex)
+    }
+
+    private fun onDocumentMofified(change: DocumentChange) {
+        if (change.oldIndex == change.newIndex) { // item changed but remained in the same position
+            snapshots[change.oldIndex] = change.document
+            notifyItemChanged(change.oldIndex)
+        } else { // item changed and changed position
+            snapshots.removeAt(change.oldIndex)
+            snapshots.add(change.newIndex, change.document)
+            notifyItemMoved(change.oldIndex, change.newIndex)
+        }
+    }
+
+    private fun onDocumentRemoved(change: DocumentChange) {
+        snapshots.removeAt(change.oldIndex)
+        notifyItemRemoved(change.oldIndex)
     }
 
 }
