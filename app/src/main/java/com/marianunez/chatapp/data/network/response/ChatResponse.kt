9786f33dbc7a306856f749441efc6824edfc6626
maria.nunez <maria.nunez@wefox.com>
2022-04-26 09:35:05 +0200

    add automatically filled id to model

--- app/src/main/java/com/marianunez/chatapp/data/network/response/ChatResponse.kt
+++ app/src/main/java/com/marianunez/chatapp/data/network/response/ChatResponse.kt
@@ -1,6 +1,9 @@
 package com.marianunez.chatapp.data.network.response
 
+import com.google.firebase.firestore.DocumentId
+
 data class ChatResponse(
+    @DocumentId // this tells Firebase that should be filled automatically with the document id when the object is created from a Firebase document
     val id: String = "",
     val name: String = "",
     val description: String = ""
